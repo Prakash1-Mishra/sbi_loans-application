a399eb1b2d4a6bb216a29f411d8fea064d726021
Prakash <universeprakash2003@gmail.com>
2026-07-19 19:55:29 +0530

    101 task completed

--- Car.java
+++ Car.java
@@ -2,8 +2,16 @@ class User{
   int userId=101;
   int userSalary=5000000000000000;
   String s="Shubhi";
+<<<<<<< Updated upstream
   
   
 //jira 102
    String s ="Kartik";
+//jira 101 task
+  public static void m1(){
+  int i=0;
+  int j=23;
+      
+
   }
+}
