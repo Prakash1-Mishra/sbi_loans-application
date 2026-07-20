36d395eda095f1b0d589d2b44e1d1e64222e5cc8
Prakash <universeprakash2003@gmail.com>
2026-07-20 08:39:35 +0530

    Revert "added variable"
    This reverts commit 63ca19e77dca27f1032ec58e591302ea2250a5af.

--- Car.java
+++ Car.java
@@ -1,6 +1,17 @@
 class User{
   int userId=101;
   int userSalary=5000000000000000;
-  String s="Kartik";
+  String s="Shubhi";
+<<<<<<< Updated upstream
+  
+  
+//jira 102
+   String s ="Kartik";
+//jira 101 task
+  public static void m1(){
+  int i=0;
+  int j=23;
+      
 
+  }
 }
