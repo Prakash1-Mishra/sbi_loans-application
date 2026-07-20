63ca19e77dca27f1032ec58e591302ea2250a5af
Prakash <universeprakash2003@gmail.com>
2026-07-20 08:23:47 +0530

    added variable

--- Car.java
+++ Car.java
@@ -1,17 +1,6 @@
 class User{
   int userId=101;
   int userSalary=5000000000000000;
-  String s="Shubhi";
-<<<<<<< Updated upstream
-  
-  
-//jira 102
-   String s ="Kartik";
-//jira 101 task
-  public static void m1(){
-  int i=0;
-  int j=23;
-      
+  String s="Kartik";
 
-  }
 }
